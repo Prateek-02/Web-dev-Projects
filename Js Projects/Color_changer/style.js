30ff7af0d3b2f685745fc9d0a64ef979b0926955
Prateek <Prateekrajgrd74@gmail.com>
2024-04-25 22:44:21 +0530

    25apr.html

--- Js Projects/Color_changer/style.js
+++ Js Projects/Color_changer/style.js
@@ -5,19 +5,25 @@ buttons.forEach(function (button) {
     console.log(button);
     button.addEventListener('click', function (e) {
         console.log(e.target)
-        if(e.target.id === 'grey'){
+        if(e.target.id === 'violet'){
             body.style.backgroundColor = e.target.id;
         }
-        if(e.target.id === 'orange'){
+        if(e.target.id === 'indigo'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'green'){
             body.style.backgroundColor = e.target.id;
         }
         if(e.target.id === 'yellow'){
             body.style.backgroundColor = e.target.id;
         }
-        if(e.target.id === 'red'){
+        if(e.target.id === 'orange'){
             body.style.backgroundColor = e.target.id;
         }
-        if(e.target.id === 'purple'){
+        if(e.target.id === 'red'){
             body.style.backgroundColor = e.target.id;
         }
     });
