4c42e9ae693c49da78e99189d9bc18ae90b000a3
Prateek <Prateekrajgrd74@gmail.com>
2024-07-23 13:13:30 +0530

    23rdjuly.js

--- Js Projects/Unlimited Colors/script.js
+++ Js Projects/Unlimited Colors/script.js
@@ -2,7 +2,7 @@ const body = document.querySelector('body');
 
 function getRandomColor(){
     let v1 = Math.ceil(Math.random()*255)
-    let v2 = Math.ceil(Math.random()*255)
+    let v2 = Math.floor(Math.random()*255)
     let v3 = Math.ceil(Math.random()*255)
 
     return `rgb(${v1},${v2},${v3})`
