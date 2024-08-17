3df5b27616339bd556bca740ba5beb1b93126c82
Prateek <Prateekrajgrd74@gmail.com>
2024-08-17 23:31:22 +0530

    1.php

---
+++ Js Projects/Random-Password-Generator/script.js
@@ -0,0 +1,14 @@
+const passBox = document.getElementById("password");
+const length  = 8;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "@#$%^&*()_+~|{}[]<>/-=";
+
+function generatePassword(){
+    let password = "";
+    password+=upperCase
+}
+
+const button = document.querySelector("button");
