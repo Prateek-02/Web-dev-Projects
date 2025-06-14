01cf33554af1bb8625c7baa2a091927b794983eb
Prateek <Prateekrajgrd74@gmail.com>
2025-06-14 11:48:06 +0530

    aitools

--- React/Ai Tools Explorer/src/components/Home.jsx
+++ React/Ai Tools Explorer/src/components/Home.jsx
@@ -1,24 +1,36 @@
 import ToolList from "./ToolList";
-import Bg_img from "../assets/Bg_img.jpg"
+import Bg_img from "../assets/Bg_img.jpg";
 import Footer from "./Footer";
-
 
 const Home = () => {
   return (
-    <div style={{ backgroundImage: `url(${Bg_img})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '65vh' }}>
-        
+    <div
+      style={{
+        backgroundImage: `url(${Bg_img})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "70vh",
+      }}
+    >
       {/* Hero Section */}
-      
-
-      <div className="text-center max-w-3xl mx-auto">
-        <h1 className="text-4xl md:text-5xl font-bold text-white pt-10">Explore the Best AI Tools</h1>
-        <p className="mt-4 text-amber-100 ">Discover AI-powered tools that enhance productivity, creativity, and efficiency.</p>
-        <h1 className="text-4xl md:text-5xl font-bold text-white pt-10 hover:text-gray-300">Your One Stop for all AI&apos;s</h1>
+      <div className="text-center max-w-3xl mx-auto pt-16">
+        <h1 className="text-4xl md:text-5xl font-bold text-white">
+          Explore the Best AI Tools
+        </h1>
+        <p className="mt-4 text-amber-100">
+          Discover AI-powered tools that enhance productivity, creativity, and
+          efficiency.
+        </p>
+        <h2 className="text-4xl md:text-5xl font-bold text-white pt-10 hover:text-gray-300">
+          Your One Stop for all AI&apos;s
+        </h2>
       </div>
 
       {/* AI Tools Section */}
       <ToolList />
-      <Footer/>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
