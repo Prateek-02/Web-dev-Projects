9530bb80398a69dfad22bf89301759b1fce104bc
Prateek <Prateekrajgrd74@gmail.com>
2025-07-22 08:33:38 +0530

    chatterHub

---
+++ ChatterHub/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const http = require('http');
+const { Server } = require('socket.io');
+
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: process.env.CLIENT_URL, credentials: true }
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
